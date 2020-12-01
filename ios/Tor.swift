109a5af0c08e86258743c885980fa986c4e9acad
Gus <gabidi@gmail.com>
2020-12-01 11:24:49 -0500

    build: [ios] Check resp codes and handle

--- ios/Tor.swift
+++ ios/Tor.swift
@@ -19,7 +19,7 @@ class Tor: NSObject {
     var service:Optional<OpaquePointer> = nil;
     var proxySocksPort:Optional<UInt16> = nil;
     var starting:Bool = false;
-
+    
     func getProxiedClient(headers:Optional<NSDictionary>,socksPort:UInt16)->URLSession{
         let config = URLSessionConfiguration.default;
         config.requestCachePolicy = URLRequest.CachePolicy.reloadIgnoringLocalCacheData;
@@ -28,14 +28,14 @@ class Tor: NSObject {
         config.connectionProxyDictionary?[kCFStreamPropertySOCKSProxyHost as String] = "127.0.0.1";
         config.connectionProxyDictionary?[kCFStreamPropertySOCKSProxyPort as String] = socksPort;
         config.connectionProxyDictionary?[kCFProxyTypeSOCKS as String] = 1;
-
+        
         if let headersPassed = headers {
             config.httpAdditionalHeaders = headersPassed as? [AnyHashable : Any]
         }
         return URLSession.init(configuration: config, delegate: nil, delegateQueue: OperationQueue.current)
     }
-
-    func prepareObjResp (data:Data,resp:HTTPURLResponse)->NSObject{
+    
+    func prepareObjResp (data:Data,resp:HTTPURLResponse,resolve: @escaping RCTPromiseResolveBlock,reject: @escaping RCTPromiseRejectBlock)->Void{
         let jsonObject: NSMutableDictionary = NSMutableDictionary()
         jsonObject.setValue(data.base64EncodedString(), forKey: "b64Data")
         jsonObject.setValue(resp.mimeType, forKey: "mimeType")
@@ -51,69 +51,72 @@ class Tor: NSObject {
                 }
             }
         }
-
-        return jsonObject as NSObject;
+        
+        
+        if 200...299 ~= resp.statusCode {
+            resolve(jsonObject as NSObject )
+        }else{
+            reject(
+                "TOR.REQUEST","Resp Code: \(resp.statusCode)",NSError.init(domain: "TOR.REQUEST", code: resp.statusCode,userInfo: jsonObject as? [String : Any]));
+        }
     }
-
+    
     @objc(request:method:jsonBody:headers:resolver:rejecter:)
     func request(url: String, method: String, jsonBody: String, headers: NSDictionary,  resolve: @escaping RCTPromiseResolveBlock,reject: @escaping RCTPromiseRejectBlock){
-
+        
         if service == nil {
             reject("TOR.SERVICE","Tor Service NOT Running. Call `startDaemon` first.",NSError.init(domain: "TOR.DAEMON", code: 99));
             return;
         }
-
+        
         let session = getProxiedClient(headers:headers,socksPort: proxySocksPort!);
         guard let _url = URL(string:url) else {
             reject("TOR.URL","Could not parse url",NSError.init(domain: "TOR", code: 404));
             return;
         }
+        
         do{
             switch method{
             case "get":
                 session.dataTask(with: _url) {  data, resp, error in
                     guard let dataResp = data , error == nil, let respData = resp else {
-                        reject("TOR.RESP",error?.localizedDescription,error);
+                        reject("TOR.GET",error?.localizedDescription,error);
                         return;
                     }
-                    resolve(self.prepareObjResp(data:dataResp,resp:respData as! HTTPURLResponse));
-                    return;
+                    self.prepareObjResp(data:dataResp,resp:respData as! HTTPURLResponse,resolve:resolve,reject:reject);
                 }.resume();
             case "delete":
                 var request = URLRequest(url:_url);
                 request.httpMethod = "DELETE";
                 session.dataTask(with: request) {  data, resp, error in
                     guard let dataResp = data , error == nil, let respData = resp else {
-                        reject("TOR.RESP",error?.localizedDescription,error);
+                        reject("TOR.DELETE",error?.localizedDescription,error);
                         return;
                     }
-                    resolve(self.prepareObjResp(data:dataResp,resp:respData as! HTTPURLResponse));
-                    return;
+                    self.prepareObjResp(data:dataResp,resp:respData as! HTTPURLResponse,resolve:resolve,reject:reject);
+                    
                 }.resume();
             case "post":
                 var request = URLRequest(url:_url);
                 request.httpMethod = "POST";
                 session.uploadTask(with: request, from: jsonBody.data(using: .utf8)) {  data, resp, error in
                     guard let dataResp = data , let respData = resp , error == nil  else {
-                        reject("TOR.RESP",error?.localizedDescription,error);
+                        reject("TOR.POST",error?.localizedDescription,error);
                         return;
                     }
-                    resolve(self.prepareObjResp(data:dataResp,resp:respData as! HTTPURLResponse));
-                    return;
+                    self.prepareObjResp(data:dataResp,resp:respData as! HTTPURLResponse,resolve:resolve,reject:reject);
                 }.resume();
-
+                
             default:
-                throw NSError.init(domain:"TOR.METHOD",code:400)
+                throw NSError.init(domain:"TOR.REQUEST_METHOD",code:400)
             }
-
+            
         } catch{
-            reject("TOR.GENERAL",error.localizedDescription,error);
-
+            reject("TOR.REQUEST",error.localizedDescription,error);
         }
-
     }
-
-
+    
+    
     @objc(startDaemon:rejecter:)
     func startDaemon( resolve: @escaping RCTPromiseResolveBlock,reject: @escaping RCTPromiseRejectBlock)->Void{
         if service != nil || starting {
@@ -122,13 +125,13 @@ class Tor: NSObject {
         }
         starting = true;
         do {
-
+            
             let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),isDirectory: true)
             // FIXME pass this and check if avalible
             let socksPort:UInt16 = 19032;
             // this gives file:///Users/.../tmp/ so we remove the file:// prefix and trailing slash
             let path = String(temporaryDirectoryURL.absoluteString.dropFirst(7).dropLast());
-
+            
             // Rust will start Tor daemon thread and block until boostrapped, so run as dispatched async task so not to block this thread
             DispatchQueue.background(background: {
                 defer {
@@ -158,7 +161,7 @@ class Tor: NSObject {
             })
         }
     }
-
+    
     @objc(getDaemonStatus:rejecter:)
     func getDaemonStatus(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock)->Void {
         guard let daemon = service else {
@@ -170,7 +173,7 @@ class Tor: NSObject {
             }
             return;
         }
-
+        
         if let status = get_status_of_owned_TorService(daemon) {
             defer {
                 destroy_cstr(status);
@@ -180,9 +183,9 @@ class Tor: NSObject {
         } else {
             reject("TOR.STATUS","UNKNOWN",NSError.init(domain: "TOR", code: 99));
         }
-
+        
     }
-
+    
     @objc(stopDaemon:rejecter:)
     func stopDaemon( resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock)->Void {
         if let hasSevice = service {
