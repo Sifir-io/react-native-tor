1ea19c75abe285f6c4699d88027456901c693424
g-homebase <gabidi@gmail.com>
2021-01-16 17:09:08 -0500

    fix: [android11-changes] update system.loadlib

--- android/src/main/java/com/reactnativetor/TorPackage.kt
+++ android/src/main/java/com/reactnativetor/TorPackage.kt
@@ -8,14 +8,25 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 import com.facebook.react.bridge.JavaScriptModule
+import android.content.pm.PackageManager
 
 class TorPackage : ReactPackage {
 
-  init {
-    System.loadLibrary("sifir_android");
-  }
+  //init {
+  //      val manager = reactContext.getPackageManager();
+  //      val ai = manager.getApplicationInfo(reactContext.getPackageName(), PackageManager.GET_META_DATA);
+  //      val bundle = ai.metaData;
+  //      val libName = bundle.getString("android.app.sifir_android");
+  //      System.loadLibrary(libName);
+  ////  System.loadLibrary("sifir_android");
+  //}
 
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    val manager = reactContext.getPackageManager();
+    val ai = manager.getApplicationInfo(reactContext.getPackageName(), PackageManager.GET_META_DATA);
+    val bundle = ai.metaData;
+    val libName = bundle.getString("android.app.sifir_android");
+    System.loadLibrary(libName);
     return Arrays.asList<NativeModule>(TorModule(reactContext))
   }
 
