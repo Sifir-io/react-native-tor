fd9c674335203ca6aa4ba348cfa09012014e7e5e
g-homebase <gabidi@gmail.com>
2021-03-02 20:22:05 -0500

    feat: [bootstrap timeout] remove debug stuff

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -21,7 +21,6 @@ export default function App() {
   ] = React.useState(15000);
   React.useEffect(() => {
     _init();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const _init = async () => {
@@ -29,13 +28,6 @@ export default function App() {
       // Init and do a few basic calls to test all is good
       console.log('App init');
       await client.startIfNotStarted();
-      console.log('Tor started');
-      await getOnion();
-      console.log('Got onion');
-      await startTcpStream();
-      console.log('started tcp stream');
-      await sendTcpMsg();
-      console.log('send tcpmsg');
     } catch (err) {
       console.error('Error starting daemon', err);
       await client.stopIfRunning();
@@ -120,7 +112,7 @@ export default function App() {
       tcpStream = conn;
       setHasStream(true);
     } catch (err) {
-      console.error('Error SendingTcpMSg', err);
+      console.error('Error StartingTcpStream', err);
     }
   };
   const closeTcpStream = async () => {
